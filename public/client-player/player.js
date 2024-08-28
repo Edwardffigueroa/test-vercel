d4d7e187c85f25fd515e081878e8232297003eb9
Edwar Fernando Figueroa Velasco <1112487607@u.icesi.edu.co>
2024-08-28 08:27:05 -0500

    new changes 2}

--- public/client-player/player.js
+++ public/client-player/player.js
@@ -7,7 +7,7 @@ async function createUser() {
       name: "Jonh Doe",
       profilePicture: "https://avatar.iran.liara.run/public/13", // if you want to generate random images for user profile go to this link: https://avatar-placeholder.iran.liara.run/
     };
-    const response = await fetch("https://client-server-vercel-deploy.vercel.app/user", {
+    const response = await fetch("https://test-vercel-seven-rose.vercel.app/user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json", // Specify the content type as JSON
